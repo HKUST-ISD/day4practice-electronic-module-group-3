1fe46e211f35be37423d20184b921614c1382754
lyfungag <lyfungag@connect.ust.hk>
2026-01-15 12:05:06 +0800

    Update Touch_Sensor_Skeleton.ino

--- Touch_Sensor_Skeleton/Touch_Sensor_Skeleton.ino
+++ Touch_Sensor_Skeleton/Touch_Sensor_Skeleton.ino
@@ -1,26 +1,26 @@
 /*Replace all the ? in the code to read the value touchRead(GPIO pin) returned.*/
 
-#define TOUCHPIN ?                    // ***Replace the ? with the GPIO pin you selected as touch pin
-#define LED ?                         // ***Replace the ? with the GPIO pin you selected to control LED
+#define TOUCHPIN 4                   // ***Replace the ? with the GPIO pin you selected as touch pin
+#define LED 2                         // ***Replace the ? with the GPIO pin you selected to control LED
 #define THRESHOLD 10000               // ***Observe the reading when the pin is touched. Then modify this threshold.***
 
 
 int touch_reading;
 
 void setup() {
-  Serial.begin(?);        // ***Start the serial communication set baud rate.***
-  pinMode(LED, ?);       // Set pin connected to LED as an output pin
+  Serial.begin(10000);        // ***Start the serial communication set baud rate.***
+  pinMode(LED, OUTPUT);       // Set pin connected to LED as an output pin
   
 }
 
 void loop() { 
-  touch_reading = touchRead(TOUCHPIN); // Read the touch sensor
+  touch_reading = touchRead(TOUCHPIN 4); // Read the touch sensor
   Serial.println(touch_reading);       // print touch_reading to the Serial monitor
   // The LED can be turned on when the pin is touched.
-  if (touch_reading ? THRESHOLD)
-    digitalWrite(?, ?);   // turn on LED
+  if (touch_reading > THRESHOLD)
+    digitalWrite(LED, HIGH);   // turn on LED
   else
-    digitalWrite(?, ?);    // turn off LED
+    digitalWrite(LED, LOW);    // turn off LED
     
-  delay(?); // delay 0.1s
+  delay(100); // delay 0.1s
 }
